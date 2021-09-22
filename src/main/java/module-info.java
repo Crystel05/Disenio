c4db2700d1ee529b7943d67f277d302a78bf9ca3
Crystel05 <crysvane05@gmail.com>
2021-09-22 10:58:58 -0600

    Revert "Nuevo"
    This reverts commit d2f780c4fed3b2f7d21d1c9dd6e52f3329b2b653.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.disenio.proyecto {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.disenio.proyecto to javafx.fxml;
+    exports com.disenio.proyecto;
+}
